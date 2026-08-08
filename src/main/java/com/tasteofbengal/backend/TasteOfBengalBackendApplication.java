c9c4c0c24b3195bc73e5098bf05ed4239640a9ca
Abhinandan Karmakar <abhinandan.karmakar.3003@gmail.com>
2026-08-08 18:32:26 +0530

    Initialize the project setup

---
+++ src/main/java/com/tasteofbengal/backend/TasteOfBengalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tasteofbengal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteOfBengalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteOfBengalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasteofbengal/backend/TasteOfBengalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasteofbengal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteOfBengalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
